7a8a102b577445032a5da5aeeb1a62624cfeff64
Pedro Rosa <pmbr3008@gmail.com>
2024-06-06 23:04:39 +0100

    Change name of file and remove not needed code

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,10 +1,6 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
 class ApiService {
   Future<bool> login(String username, String password) async {
-    // Implement your login API call here
-    // Example:
-    await Future.delayed(Duration(seconds: 2)); // Simulate network delay
-    return username == 'user' && password == 'password'; // Dummy authentication
+    await Future.delayed(Duration(seconds: 2));
+    return username == 'user' && password == 'password';
   }
 }
